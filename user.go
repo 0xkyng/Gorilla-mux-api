b8f8f1bfdf072cedf278e7d004225aceb9d3f66b
Isaac Wanger <teryima127@gmail.com>
2022-12-27 09:35:44 +0100

    connected to db

--- user.go
+++ user.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
 )
+
+var DB *gorm.DB
+var err error
+
+const DNS = "root:G8keeper@mysql@tcp(127.0.0.1:3306)/gorrila-mux-api?charset=utf8mb4&parseTime=True&loc=Local"
 
 
 type User struct{
@@ -12,6 +19,14 @@ type User struct{
 	FirstName  string `json:"firstname"`
 	LastName   string  `json:"lastname"`
 	Email      string   `json:"email"`
+}
+
+func InitialMigration() {
+	DB, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to DB")
+	}
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
